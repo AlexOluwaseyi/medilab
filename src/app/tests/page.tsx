85be299a74b801136cda5dcef7c5f1da772c5fbe
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2025-04-20 16:22:50 +0100

    Patch: Added page for tests '/tests' from previous project

---
+++ src/app/tests/page.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Tests from "@/components/Tests";
+
+const Page = () => {
+  return (
+    <>
+      <Header />
+      <Tests />
+      <Footer />
+    </>
+  );
+};
+export default Page;
